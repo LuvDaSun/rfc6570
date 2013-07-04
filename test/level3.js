df5362e062d261c530deb7aa594d8c082cba23ea
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-07-04 14:49:18 +0200

    implements level 3 parsing

--- test/level3.js
+++ test/level3.js
@@ -12,57 +12,57 @@ describe('Level 3', function(){
 	
 	describe('String expansion with multiple variables', function(){
 		
-		h.addTest('map?{x,y}', 'map?1024,768', data);
-		h.addTest('{x,hello,y}', '1024,Hello%20World%21,768', data);
+		h.addTest('map?{x,y}', 'map?1024,768', data, true);
+		h.addTest('{x,hello,y}', '1024,Hello%20World%21,768', data, true);
 
 	});
 
 	describe('Reserved expansion with multiple variables', function(){
 
-		h.addTest('{+x,hello,y}', '1024,Hello%20World!,768', data);
-		h.addTest('{+path,x}/here', '/foo/bar,1024/here', data);
+		h.addTest('{+x,hello,y}', '1024,Hello%20World!,768', data, true);
+		h.addTest('{+path,x}/here', '/foo/bar,1024/here', data, true);
 		
 	});
 
 	describe('Fragment expansion with multiple variables', function(){
 
-		h.addTest('{#x,hello,y}', '#1024,Hello%20World!,768', data);
-		h.addTest('{#path,x}/here', '#/foo/bar,1024/here', data);
+		h.addTest('{#x,hello,y}', '#1024,Hello%20World!,768', data, true);
+		h.addTest('{#path,x}/here', '#/foo/bar,1024/here', data, true);
 		
 	});
 
 	describe('Label expansion, dot-prefixed', function(){
 
-		h.addTest('X{.var}', 'X.value', data);
-		h.addTest('X{.x,y}', 'X.1024.768', data);
+		h.addTest('X{.var}', 'X.value', data, true);
+		h.addTest('X{.x,y}', 'X.1024.768', data, true);
 		
 	});
 
 	describe('Path segments, slash-prefixed', function(){
 
-		h.addTest('{/var}', '/value', data);
-		h.addTest('{/var,x}/here', '/value/1024/here', data);
+		h.addTest('{/var}', '/value', data, true);
+		h.addTest('{/var,x}/here', '/value/1024/here', data, true);
 		
 	});
 
 	describe('Path-style parameters, semicolon-prefixed', function(){
 
-		h.addTest('{;x,y}', ';x=1024;y=768', data);
-		h.addTest('{;x,y,empty}', ';x=1024;y=768;empty', data);
+		h.addTest('{;x,y}', ';x=1024;y=768', data, true);
+		h.addTest('{;x,y,empty}', ';x=1024;y=768;empty', data, true);
 	
 	});
 
 	describe('Form-style query, ampersand-separated', function(){
 
-		h.addTest('{?x,y}', '?x=1024&y=768', data);
-		h.addTest('{?x,y,empty}', '?x=1024&y=768&empty=', data);
+		h.addTest('{?x,y}', '?x=1024&y=768', data, true);
+		h.addTest('{?x,y,empty}', '?x=1024&y=768&empty=', data, true);
 		
 	});
 
 	describe('Form-style query continuation', function(){
 
-		h.addTest('?fixed=yes{&x}', '?fixed=yes&x=1024', data);
-		h.addTest('{&x,y,empty}', '&x=1024&y=768&empty=', data);
+		h.addTest('?fixed=yes{&x}', '?fixed=yes&x=1024', data, true);
+		h.addTest('{&x,y,empty}', '&x=1024&y=768&empty=', data, true);
 		
 	});
 
