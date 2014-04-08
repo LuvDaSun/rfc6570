4de383f3c8a2d1defcc51129a6f9c9a37a8bbbfa
Elmer Bulthuis <elmerbulthuis@gmail.com>
2014-04-08 20:02:01 +0200

    make

--- rfc6570.js
+++ rfc6570.js
@@ -1,136 +1,139 @@
 !function(e){"object"==typeof exports?module.exports=e():"function"==typeof define&&define.amd?define(e):"undefined"!=typeof window?window.rfc6570=e():"undefined"!=typeof global?global.rfc6570=e():"undefined"!=typeof self&&(self.rfc6570=e())}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* jshint node:true */
+
 var UriTemplate = require('./UriTemplate');
 
-function Router(){
-	var routes = [];
+function Router() {
+    var routes = [];
 
-	this.add = function(template, handler) {
+    this.add = function (template, handler) {
 
-		routes.push({
-			template: new UriTemplate(template)
-			, handler: handler
-		})//
+        routes.push({
+            template: new UriTemplate(template),
+            handler: handler
+        }); //
 
-	}//add
+    }; //add
 
-	this.handle = function(url) {
-	
-		return routes.some(function(route){
-			var data = route.template.parse(url);
-			return data && route.handler(data) !== false;
-		});
+    this.handle = function (url) {
 
-	}//exec
+        return routes.some(function (route) {
+            var data = route.template.parse(url);
+            return data && route.handler(data) !== false;
+        });
 
-}//Router
+    }; //exec
+
+} //Router
 
 module.exports = Router;
-
 },{"./UriTemplate":2}],2:[function(require,module,exports){
+/* jshint node:true */
+
 module.exports = UriTemplate;
 
 
 var operatorOptions = {
-	"": {
-		"prefix": ""
-		, "seperator": ","
-		, "assignment": false
-		, "assignEmpty": false
-		, "encode": percentEncode
-	}
-	, "+": {
-		"prefix": ""
-		, "seperator": ","
-		, "assignment": false
-		, "assignEmpty": false
-		, "encode": encodeURI
-	}
-	, "#": {
-		"prefix": "#"
-		, "seperator": ","
-		, "assignment": false
-		, "assignEmpty": false
-		, "encode": encodeURI
-	}
-	, ".": {
-		"prefix": "."
-		, "seperator": "."
-		, "assignment": false
-		, "assignEmpty": false
-		, "encode": percentEncode
-	}
-	, "/": {
-		"prefix": "/"
-		, "seperator": "/"
-		, "assignment": false
-		, "encode": encodeURIComponent
-	}
-	, ";": {
-		"prefix": ";"
-		, "seperator": ";"
-		, "assignment": true
-		, "assignEmpty": false
-		, "encode": encodeURIComponent
-	}
-	, "?": {
-		"prefix": "?"
-		, "seperator": "&"
-		, "assignment": true
-		, "assignEmpty": true
-		, "encode": encodeURIComponent
-	}
-	, "&": {
-		"prefix": "&"
-		, "seperator": "&"
-		, "assignment": true
-		, "assignEmpty": true
-		, "encode": encodeURIComponent
-	}
-};//operatorOptions
+    "": {
+        "prefix": "",
+        "seperator": ",",
+        "assignment": false,
+        "assignEmpty": false,
+        "encode": percentEncode
+    },
+    "+": {
+        "prefix": "",
+        "seperator": ",",
+        "assignment": false,
+        "assignEmpty": false,
+        "encode": encodeURI
+    },
+    "#": {
+        "prefix": "#",
+        "seperator": ",",
+        "assignment": false,
+        "assignEmpty": false,
+        "encode": encodeURI
+    },
+    ".": {
+        "prefix": ".",
+        "seperator": ".",
+        "assignment": false,
+        "assignEmpty": false,
+        "encode": percentEncode
+    },
+    "/": {
+        "prefix": "/",
+        "seperator": "/",
+        "assignment": false,
+        "encode": encodeURIComponent
+    },
+    ";": {
+        "prefix": ";",
+        "seperator": ";",
+        "assignment": true,
+        "assignEmpty": false,
+        "encode": encodeURIComponent
+    },
+    "?": {
+        "prefix": "?",
+        "seperator": "&",
+        "assignment": true,
+        "assignEmpty": true,
+        "encode": encodeURIComponent
+    },
+    "&": {
+        "prefix": "&",
+        "seperator": "&",
+        "assignment": true,
+        "assignEmpty": true,
+        "encode": encodeURIComponent
+    }
+}; //operatorOptions
 
-function percentEncode(value){
-	/*
+function percentEncode(value) {
+    /*
 	http://tools.ietf.org/html/rfc3986#section-2.3
 	*/
-	var unreserved = "-._~";
+    var unreserved = "-._~";
 
-	if(isUndefined(value)) return '';
+    if (isUndefined(value)) return '';
 
-	value = value.toString();
+    value = value.toString();
 
-	return Array.prototype.map.call(value, function(ch) {
-		var charCode = ch.charCodeAt(0);
+    return Array.prototype.map.call(value, function (ch) {
+        var charCode = ch.charCodeAt(0);
 
-		if(charCode >= 0x30 && charCode <= 0x39) return ch
-		if(charCode >= 0x41 && charCode <= 0x5a) return ch
-		if(charCode >= 0x61 && charCode <= 0x7a) return ch
+        if (charCode >= 0x30 && charCode <= 0x39) return ch;
+        if (charCode >= 0x41 && charCode <= 0x5a) return ch;
+        if (charCode >= 0x61 && charCode <= 0x7a) return ch;
 
-		if(~unreserved.indexOf(ch)) return ch;
+        if (~unreserved.indexOf(ch)) return ch;
 
-		return '%' + charCode.toString(16).toUpperCase();
-	}).join('');
+        return '%' + charCode.toString(16).toUpperCase();
+    }).join('');
 
-}//percentEncode
+} //percentEncode
 
-function isDefined(value){
-	return !isUndefined(value);
-}//isDefined
-function isUndefined(value){
-	/*
+function isDefined(value) {
+    return !isUndefined(value);
+} //isDefined
+function isUndefined(value) {
+    /*
 	http://tools.ietf.org/html/rfc6570#section-2.3
 	*/
-	if(value === null) return true;
-	if(value === undefined) return true;
-	if(Array.isArray(value)) {
-		if(value.length === 0) return true;
-	}
+    if (value === null) return true;
+    if (value === undefined) return true;
+    if (Array.isArray(value)) {
+        if (value.length === 0) return true;
+    }
 
-	return false;
-}//isUndefined
+    return false;
+} //isUndefined
 
 
-function UriTemplate(template){
-	/*
+function UriTemplate(template) {
+    /*
 	http://tools.ietf.org/html/rfc6570#section-2.2
 
 	expression    =  "{" [ operator ] variable-list "}"
@@ -139,210 +142,210 @@ function UriTemplate(template){
 	op-level3     =  "." / "/" / ";" / "?" / "&"
 	op-reserve    =  "=" / "," / "!" / "@" / "|"
 	*/
-	var reTemplate = /\{([\+#\.\/;\?&=\,!@\|]?)([A-Za-z0-9_\,\.\:\*]+?)\}/g;
-	var reVariable = /^([\$_a-z][\$_a-z0-9]*)((?:\:[1-9][0-9]?[0-9]?[0-9]?)?)(\*?)$/i;
-	var match;
-	var pieces = [];
-	var glues = [];
-	var offset = 0;
-	var pieceCount = 0;
+    var reTemplate = /\{([\+#\.\/;\?&=\,!@\|]?)([A-Za-z0-9_\,\.\:\*]+?)\}/g;
+    var reVariable = /^([\$_a-z][\$_a-z0-9]*)((?:\:[1-9][0-9]?[0-9]?[0-9]?)?)(\*?)$/i;
+    var match;
+    var pieces = [];
+    var glues = [];
+    var offset = 0;
+    var pieceCount = 0;
 
-	while(match = reTemplate.exec(template)){
-		glues.push(template.substring(offset, match.index));
-		/*
+    while ( !! (match = reTemplate.exec(template))) {
+        glues.push(template.substring(offset, match.index));
+        /*
 		The operator characters equals ("="), comma (","), exclamation ("!"),
 		at sign ("@"), and pipe ("|") are reserved for future extensions.
 		*/
-		if(match[1] && ~'=,!@|'.indexOf(match[1])){
-			throw "operator '" + match[1] + "' is reserved for future extensions";
-		}
+        if (match[1] && ~'=,!@|'.indexOf(match[1])) {
+            throw "operator '" + match[1] + "' is reserved for future extensions";
+        }
 
-		offset = match.index;
-		pieces.push({
-			operator: match[1]
-			, variables: match[2].split(',').map(function(variable){
-				var match = reVariable.exec(variable)
-				return {
-					name: match[1]
-					, maxLength: match[2] && parseInt(match[2].substring(1), 10)
-					, composite: !!match[3]
-				};
-			})
-		});
-		offset += match[0].length;
-		pieceCount++;
-	}
-	glues.push(template.substring(offset));
+        offset = match.index;
+        pieces.push({
+            operator: match[1],
+            variables: match[2].split(',').map(variableMapper)
+        });
+        offset += match[0].length;
+        pieceCount++;
+    }
 
-	this.parse = function (str){
-		var data = {};
-		var offset = 0;
-		var offsets = [];
+    function variableMapper(variable) {
+        var match = reVariable.exec(variable);
+        return {
+            name: match[1],
+            maxLength: match[2] && parseInt(match[2].substring(1), 10),
+            composite: !! match[3]
+        };
+    }
 
-		if(!glues.every(function (glue, glueIndex) {
-			var index;
-			if(glueIndex > 0 && glue === '') index = str.length;
-			else index = str.indexOf(glue, offset);
-			
-			offset = index;
-			offsets.push(offset);
-			offset += glue.length;
-			
-			return ~index;
-		})) return false;
+    glues.push(template.substring(offset));
 
-		if(!pieces.every(function (piece, pieceIndex) {
-			var options = operatorOptions[piece.operator];
-			var value, values;
-			var offsetBegin = offsets[pieceIndex] + glues[pieceIndex].length;
-			var offsetEnd = offsets[pieceIndex + 1];
+    this.parse = function (str) {
+        var data = {};
+        var offset = 0;
+        var offsets = [];
 
-			value = str.substring(offsetBegin, offsetEnd);
-			if(value.length === 0) return true;
-			if(value.substring(0, options.prefix.length) !== options.prefix) return false;
-			value = value.substring(options.prefix.length);
-			values = value.split(options.seperator);
+        if (!glues.every(function (glue, glueIndex) {
+            var index;
+            if (glueIndex > 0 && glue === '') index = str.length;
+            else index = str.indexOf(glue, offset);
 
-			if(!piece.variables.every(function(variable, variableIndex){
-				var value = values[variableIndex];
-				var name;
+            offset = index;
+            offsets.push(offset);
+            offset += glue.length;
 
-				if(value === undefined) return true;
+            return~ index;
+        })) return false;
 
-				name = variable.name;
-				
-				if(options.assignment) {
-					if(value.substring(0, name.length) !== name) return false;
-					value = value.substring(name.length);
-					if(value.length === 0 && options.assignEmpty) return false;
-					if(value.length > 0) {
-						if(value[0] !== '=') return false;
-						value = value.substring(1);
-					}
-				}
-				value = decodeURIComponent(value);
-				data[name] = value;
+        if (!pieces.every(function (piece, pieceIndex) {
+            var options = operatorOptions[piece.operator];
+            var value, values;
+            var offsetBegin = offsets[pieceIndex] + glues[pieceIndex].length;
+            var offsetEnd = offsets[pieceIndex + 1];
 
-				return true;
-			})) return false;
+            value = str.substring(offsetBegin, offsetEnd);
+            if (value.length === 0) return true;
+            if (value.substring(0, options.prefix.length) !== options.prefix) return false;
+            value = value.substring(options.prefix.length);
+            values = value.split(options.seperator);
 
-			return true;
+            if (!piece.variables.every(function (variable, variableIndex) {
+                var value = values[variableIndex];
+                var name;
 
-		})) return false;
+                if (value === undefined) return true;
 
-		return data;
-	}//parse
+                name = variable.name;
 
-	this.stringify = function (data){
-		var str = '';
-		
-		str += glues[0];
-		if(!pieces.every(function (piece, pieceIndex) {
+                if (options.assignment) {
+                    if (value.substring(0, name.length) !== name) return false;
+                    value = value.substring(name.length);
+                    if (value.length === 0 && options.assignEmpty) return false;
+                    if (value.length > 0) {
+                        if (value[0] !== '=') return false;
+                        value = value.substring(1);
+                    }
+                }
+                value = decodeURIComponent(value);
+                data[name] = value;
 
-			var options = operatorOptions[piece.operator];
-			var parts;
+                return true;
+            })) return false;
 
-			parts = piece.variables.map(function(variable){
-				var value = data[variable.name];
+            return true;
 
-				if(!Array.isArray(value)) value = [value];
+        })) return false;
 
-				value = value.filter(isDefined);
+        return data;
+    }; //parse
 
-				if(isUndefined(value)) return null;
+    this.stringify = function (data) {
+        var str = '';
+        data = data || {};
 
-				if(variable.composite){
-					value = value.map(function(value){
-					
-						if(typeof value === 'object') {
-							
-							value = Object.keys(value).map(function(key){
-								var keyValue = value[key];
-								if(variable.maxLength) keyValue = keyValue.substring(0, variable.maxLength);
-								
-								keyValue = options.encode(keyValue);
+        str += glues[0];
+        if (!pieces.every(function (piece, pieceIndex) {
 
-								if(keyValue) keyValue = key + '=' + keyValue;
-								else {
-									keyValue = key;
-									if(options.assignEmpty) keyValue += '=';
-								}
+            var options = operatorOptions[piece.operator];
+            var parts;
 
-								return keyValue;
-							}).join(options.seperator)
+            parts = piece.variables.map(function (variable) {
+                var value = data[variable.name];
 
-						}
-						else {
-							if(variable.maxLength) value = value.substring(0, variable.maxLength);
-							
-							value = options.encode(value);
+                if (!Array.isArray(value)) value = [value];
 
-							if(options.assignment) {
-								if(value) value = variable.name + '=' + value;
-								else {
-									value = variable.name;
-									if(options.assignEmpty) value += '=';
-								}
-							}
-						}
+                value = value.filter(isDefined);
 
-						return value;
-					});
-					
-					value = value.join(options.seperator);
-				}
-				else {
-					value = value.map(function(value){
-						if(typeof value === 'object') {
-							return Object.keys(value).map(function(key){
-								var keyValue = value[key];
-								if(variable.maxLength) keyValue = keyValue.substring(0, variable.maxLength);
-								return key + ',' + options.encode(keyValue);
-							}).join(',')
-						}
-						else {
-							if(variable.maxLength) value = value.substring(0, variable.maxLength);
-							
-							return options.encode(value);
-						}
+                if (isUndefined(value)) return null;
 
-					})
-					value = value.join(',');
-					
-					if(options.assignment) {
-						if(value) value = variable.name + '=' + value;
-						else {
-							value = variable.name;
-							if(options.assignEmpty) value += '=';
-						}
-					}
+                if (variable.composite) {
+                    value = value.map(function (value) {
 
-				}					
+                        if (typeof value === 'object') {
 
-				return value;
-			});
+                            value = Object.keys(value).map(function (key) {
+                                var keyValue = value[key];
+                                if (variable.maxLength) keyValue = keyValue.substring(0, variable.maxLength);
 
-			parts = parts.filter(isDefined);
-			if(isDefined(parts)) {
-				str += options.prefix;
-				str += parts.join(options.seperator);
-			}
+                                keyValue = options.encode(keyValue);
 
-			str += glues[pieceIndex + 1];
-			return true;
-		})) return false;
+                                if (keyValue) keyValue = key + '=' + keyValue;
+                                else {
+                                    keyValue = key;
+                                    if (options.assignEmpty) keyValue += '=';
+                                }
 
-		return str;
-	}//stringify
+                                return keyValue;
+                            }).join(options.seperator);
 
-}//UriTemplate
+                        } else {
+                            if (variable.maxLength) value = value.substring(0, variable.maxLength);
 
+                            value = options.encode(value);
 
+                            if (options.assignment) {
+                                if (value) value = variable.name + '=' + value;
+                                else {
+                                    value = variable.name;
+                                    if (options.assignEmpty) value += '=';
+                                }
+                            }
+                        }
 
+                        return value;
+                    });
+
+                    value = value.join(options.seperator);
+                } else {
+                    value = value.map(function (value) {
+                        if (typeof value === 'object') {
+                            return Object.keys(value).map(function (key) {
+                                var keyValue = value[key];
+                                if (variable.maxLength) keyValue = keyValue.substring(0, variable.maxLength);
+                                return key + ',' + options.encode(keyValue);
+                            }).join(',');
+                        } else {
+                            if (variable.maxLength) value = value.substring(0, variable.maxLength);
+
+                            return options.encode(value);
+                        }
+
+                    });
+                    value = value.join(',');
+
+                    if (options.assignment) {
+                        if (value) value = variable.name + '=' + value;
+                        else {
+                            value = variable.name;
+                            if (options.assignEmpty) value += '=';
+                        }
+                    }
+
+                }
+
+                return value;
+            });
+
+            parts = parts.filter(isDefined);
+            if (isDefined(parts)) {
+                str += options.prefix;
+                str += parts.join(options.seperator);
+            }
+
+            str += glues[pieceIndex + 1];
+            return true;
+        })) return false;
+
+        return str;
+    }; //stringify
+
+} //UriTemplate
 },{}],3:[function(require,module,exports){
+/* jshint node:true */
+
 module.exports = {
-	Router: require('./Router'),
-	UriTemplate: require('./UriTemplate')
+    Router: require('./Router'),
+    UriTemplate: require('./UriTemplate')
 };
 },{"./Router":1,"./UriTemplate":2}]},{},[3])
 (3)
